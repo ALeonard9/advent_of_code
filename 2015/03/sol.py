cae9b05dc7584070d50a33d4f0dec6f562744e57
ALeonard9 <aleonard9@hotmail.com>
2022-12-07 22:02:52 +0530

    2015 day 3

--- 2015/03/sol.py
+++ 2015/03/sol.py
@@ -5,21 +5,65 @@ import importlib.util
 import importlib.machinery
 
 def p1(f):
-    ans = 0
+    ans = 1
+    coordinates = ["0,0"]
+    x = 0
+    y = 0
     with open(f) as file:
         for line in file:
             line = line.strip()
+            for sym in line:
+                x,y = mover(sym,x,y)
+                curr = "{},{}".format(x,y)
+                if not curr in coordinates:
+                    coordinates.append(curr)
+                    ans += 1
 
     return ans
 
 
 def p2(f):
-    ans = 0
+    ans = 1
+    coordinates = ["0,0"]
+    santa = GiftGiver()
+    robo_santa = GiftGiver()
+    i=0
     with open(f) as file:
         for line in file:
             line = line.strip()
 
+            for sym in line:
+                i += 1
+                if (i % 2) == 0:
+                    s = santa
+                else:
+                    s = robo_santa
+                s.move(sym)
+                curr = "{},{}".format(s.x,s.y)
+                if not curr in coordinates:
+                    coordinates.append(curr)
+                    ans += 1
     return ans
+
+class GiftGiver:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+    def move(self,sym):
+        self.x,self.y = mover(sym,self.x,self.y)
+
+
+def mover(sym,x,y):
+    match sym:
+        case '^':
+            x += 1
+        case '>':
+            y += 1
+        case 'v':
+            x -= 1
+        case '<':
+            y -= 1
+    return x,y
 
 def import_path(path):
     module_name = os.path.basename(path).replace('-', '_')
