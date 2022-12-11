6a4b49a9fe00b83181f55b4d34fb6eab16e38814
ALeonard9 <aleonard9@hotmail.com>
2022-12-11 19:25:17 +0530

    Day 10 fixed

--- 2022/10/sol.py
+++ 2022/10/sol.py
@@ -18,29 +18,51 @@ def p1(f):
                 cycle, x, ans = cycle_through(cycle, x, ans, amount)
             else:
                 cycle, x, ans = cycle_through(cycle, x, ans)
-
-
     return ans
 
 
 def p2(f):
-    ans = 0
+    ans = {}
+    cycle = 1
+    x = 1
+    row = 1
+    ans[row] = ""
     with open(f) as file:
         for line in file:
             line = line.strip()
-
-    return ans
+            if line.startswith('a'):
+                _, amount = line.split()
+                amount = int(amount)
+                cycle, x, ans, row = draw(cycle, x, ans, row)
+                cycle, x, ans, row = draw(cycle, x, ans, row, amount)
+            else:
+                cycle, x, ans, row = draw(cycle, x, ans, row)
+    draw_ans(ans)
+    return ans[1]
 
 def cycle_through(cycle, x, ans, amount=0):
-    # logger.log("BEGIN: Cycle {}".format(cycle))
     if cycle == 20 or (cycle-20) % 40 == 0:
         ans +=  cycle * x
-        logger.log("{},{},{}".format(cycle,x,ans))
     x += amount
-    # logger.log("{},{},{}".format(cycle,x,ans))
-    # logger.log("END Cycle {}".format(cycle))
     cycle += 1
     return cycle, x, ans
+
+def draw(cycle, x, ans, row, amount=0):
+    position = cycle-(40*(row-1)) -1
+    if position in range(x-1,x+2):
+        ans[row] += "#"
+    else:    
+        ans[row] += "."
+    x += amount
+    cycle += 1
+    if cycle % 40 == 1: 
+        row += 1
+        if row < 7: ans[row] = ""
+    return cycle, x, ans, row
+
+def draw_ans(ans):
+    for key in ans:
+        print(ans[key])
 
 def import_path(path):
     module_name = os.path.basename(path).replace('-', '_')
